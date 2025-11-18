2ed776385f7cb630b43c5347e1c4d13960f06f75
Filip Madunicky <yelidric@gmail.com>
2025-11-19 00:31:47 +0100

    Adds login functionality to blog app tests
    Adds tests for successful login and failed login attempts with invalid credentials.
    
    Resets the database and creates a user before each test to ensure a consistent testing environment.

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { describe, test, expect, beforeEach } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({page}) => {
+  beforeEach(async ({page, request}) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        username: 'yelino',
+        name: 'Filip Madunicky',
+        password: 'heslo123'
+      }
+    })
+
     await page.goto('/')
   })
 
@@ -10,5 +19,26 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', {name: 'login'})).toBeVisible()
+  })
+
+  
+  describe('Login', () => {
+    test('user can login successfuly', async ({page}) => {
+      await page.getByLabel('username').fill('yelino')
+      await page.getByLabel('password').fill('heslo123')
+      await page.getByRole('button', {name: 'login'}).click()
+
+      await expect(page.getByText('Filip Madunicky logged in')).toBeVisible()
+      await expect(page.getByRole('button', {name: 'logout'})).toBeVisible()
+      await expect(page.locator('.notification.ok')).toContainText('user successfuly logged in')
+    })
+
+    test('fails with wrong credentials', async ({page}) => {
+      await page.getByLabel('username').fill('yelino')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', {name: 'login'}).click()
+
+      await expect(page.locator('.notification.error')).toContainText('invalid username or password')
+    })
   })
 })
