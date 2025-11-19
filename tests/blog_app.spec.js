38ee3eaf6c07906f2afb08ea7da91a6230e62bcb
Filip Madunicky <yelidric@gmail.com>
2025-11-20 00:14:51 +0100

    Adds test case for blog creation
    Adds an end-to-end test case to verify that a logged-in user can successfully create a new blog post. This test case covers the scenario of logging in, navigating to the blog creation form, filling out the required fields, submitting the form, and verifying that the blog post is created and displayed correctly with a success notification.

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -41,4 +41,23 @@ describe('Blog app', () => {
       await expect(page.locator('.notification.error')).toContainText('invalid username or password')
     })
   })
+
+  describe('when logged in', () => {
+    beforeEach(async ({page}) => {
+      await page.getByLabel('username').fill('yelino')
+      await page.getByLabel('password').fill('heslo123')
+      await page.getByRole('button', {name: 'login'}).click()
+    })
+
+    test('user can create blog', async ({page}) => {
+      await page.getByRole('button', {name: 'create new blog'}).click()
+
+      await page.getByLabel('title').fill('New testing blog')
+      await page.getByLabel('url').fill('url for testing blog')
+      await page.getByRole('button', {name: 'create'}).click()
+
+      await expect(page.locator('.notification.ok')).toContainText('a new blog New testing blog by Filip Madunicky added')
+      await expect(page.locator('.post')).toContainText('New testing blog')
+    })
+  })
 })
